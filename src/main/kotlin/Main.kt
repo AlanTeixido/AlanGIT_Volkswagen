25efd049c8c0629f53e12e4282a3b7e30ad5a75e
alanteixido <alan.teixido.7e7@itb.cat>
2023-12-22 12:44:10 +0100

    Codig en funcionament perfecte i comentat amb KDoc

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,61 +1,116 @@
 import org.example.*
 
-fun main(){
+/**
+ * Función principal que ejecuta el programa.
+ */
+fun main() {
+    mostrarAsciiArt()
     menu()
-    val normalOCamper:Boolean = pedirVersionFurgo()
-    val suma250:Boolean = pedirTienePortaBicis()
-    val sumaYa250:Int
-    if (suma250) sumaYa250=250
-    else sumaYa250=0
-    val precioBase=calcularPreuBase(normalOCamper)+sumaYa250
-    val precioActual=calcularPreuActual(calcularPreuBase(normalOCamper))+sumaYa250
-    println(BLACK_BOLD+ WHITE_UNDERLINED +"El precio base de tu furgoneta es: "+precioBase+"€"+RESET)
-    println(BLACK_BOLD+ WHITE_UNDERLINED +"El precio actual de tu furgoneta es: "+precioActual+"€"+ RESET)
+    val versionNormal = pedirVersionFurgo()
+    val tienePortaBicis = pedirTienePortaBicis()
+
+    // Calcula el precio base sumando 250 si tiene portabicis.
+    val precioBase = calcularPrecioBase(versionNormal) + if (tienePortaBicis) 250 else 0
+
+    // Calcula el precio actual sumando 250 si tiene portabicis.
+    val precioActual = calcularPrecioActual(calcularPrecioBase(versionNormal)) + if (tienePortaBicis) 250 else 0
+
+    println(BLACK_BOLD + WHITE_UNDERLINED + "El precio base de tu furgoneta es: $precioBase €" + RESET)
+    println(BLACK_BOLD + WHITE_UNDERLINED + "El precio actual de tu furgoneta es: $precioActual €" + RESET)
 }
 
-fun menu(){
+/**
+ * Muestra un arte ASCII en la consola.
+ * Este arte ASCII es un ejemplo y puede personalizarse según las preferencias.
+ */
+fun mostrarAsciiArt() {
+    println(
+        """
+                                                         _________________________   
+                    /\\      _____          _____       |   |     |     |    | |  \  
+     ,-----,       /  \\____/__|__\_    ___/__|__\___   |___|_____|_____|____|_|___\ 
+  ,--'---:---`--, /  |  _     |     `| |      |      `| |                    | |    \
+ ==(o)-----(o)==J    `(o)-------(o)=   `(o)------(o)'   `--(o)(o)--------------(o)--'  
+````````````````````````````~~````````````````````````````````````````````````````
+    """
+    )
+}
+
+/**
+ * Muestra un mensaje de bienvenida en la consola.
+ */
+fun menu() {
     println("Bienvenido al mercado de vehículos")
-
 }
 
-fun pedirVersionFurgo():Boolean{
-    println(CYAN+"¿Tu furgoneta es la versión normal (true) o la versión Camper (false)?"+RESET)
-    var versionFurgo= readBoolean(BLUE+"Introduce la versión de tu furgoneta:"+RESET, RED +"introduce true/false" + RESET)
-    if (versionFurgo){
-        println(PURPLE+"Tienes la furgoneta normal"+RESET)
-    }else{
-        println(PURPLE+"Tienes la versión Camper"+RESET)
+/**
+ * Pide al usuario la versión de la furgoneta (normal o Camper) y devuelve un booleano.
+ *
+ * @return true si la furgoneta es la versión normal, false si es la versión Camper.
+ */
+fun pedirVersionFurgo(): Boolean {
+    println(CYAN + "¿Tu furgoneta es la versión normal (true) o la versión Camper (false)?"+RESET)
+    var versionFurgo = readBoolean(BLUE + "Introduce la versión de tu furgoneta:" + RESET, RED + "introduce true/false" + RESET)
+
+    // Muestra un mensaje indicando la versión de la furgoneta.
+    if (versionFurgo) {
+        println(GREEN + "Tienes la furgoneta normal" + RESET)
+    } else {
+        println(GREEN + "Tienes la versión Camper" + RESET)
     }
     return versionFurgo
 }
 
-fun pedirTienePortaBicis():Boolean{
-    var portaBicis=readBoolean(BLUE+"¿Tiene portabicis tu furgoneta?:"+RESET,"introduce true/false")
-    if (portaBicis) println(PURPLE+"Tu furgoneta tiene portabicis"+RESET)
-    else println(PURPLE+"Tu furgoneta no tiene portabicis"+RESET)
+/**
+ * Pide al usuario si la furgoneta tiene portabicis y devuelve un booleano.
+ *
+ * @return true si la furgoneta tiene portabicis, false si no.
+ */
+fun pedirTienePortaBicis(): Boolean {
+    var portaBicis = readBoolean(BLUE + "¿Tiene portabicis tu furgoneta?:" + RESET, RED + "introduce true/false" + RESET)
+
+    // Muestra un mensaje indicando si la furgoneta tiene portabicis o no.
+    if (portaBicis) println(GREEN + "Tu furgoneta tiene portabicis" + RESET)
+    else println(GREEN + "Tu furgoneta no tiene portabicis" + RESET)
     return portaBicis
 }
 
-fun depreciacionEstadoNeumaticos():Int{
-    var estadoNeumaticos=llegirLong(BLUE+"Introduce los km recorridos con los neumáticos acutales:"+RESET,0)
-    println(PURPLE+"Los neumáticos actuales de tu furgoneta han reccorrido "+estadoNeumaticos+"km"+RESET)
-    var depreciacion:Int
-    if (estadoNeumaticos<5000){
-        depreciacion=0
-    }else if (estadoNeumaticos>=5000 && estadoNeumaticos<10000){
-        depreciacion=200
-    }else{
-        depreciacion=300
+/**
+ * Pide al usuario los kilómetros recorridos con los neumáticos actuales y devuelve la depreciación correspondiente.
+ *
+ * @return La depreciación de los neumáticos.
+ */
+fun depreciacionEstadoNeumaticos(): Int {
+    var estadoNeumaticos = llegirLong(BLUE + "Introduce los km recorridos con los neumáticos acutales:" + RESET, 0)
+
+    // Muestra un mensaje indicando los kilómetros recorridos por los neumáticos.
+    println(GREEN + "Los neumáticos actuales de tu furgoneta han recorrido $estadoNeumaticos km" + RESET)
+
+    var depreciacion: Int
+    if (estadoNeumaticos < 5000) {
+        depreciacion = 0
+    } else if (estadoNeumaticos >= 5000 && estadoNeumaticos < 10000) {
+        depreciacion = 200
+    } else {
+        depreciacion = 300
     }
     return depreciacion
 }
 
-fun porcentajeKM():Double{
-    var porcentaje:Double
-    var anosFurgo=llegirInt(BLUE+"Introduce los años de tu furgoneta:"+RESET,0,100)
-    println(PURPLE+"Tu furgoneta lleva matriculada "+anosFurgo+" años"+RESET)
-    if (anosFurgo<6) porcentaje=0.00001
-    else if (anosFurgo>=6 && anosFurgo<10) porcentaje=0.00002
-    else porcentaje=0.00004
+/**
+ * Pide al usuario los años de la furgoneta y devuelve el porcentaje correspondiente.
+ *
+ * @return El porcentaje de descuento basado en los años de la furgoneta.
+ */
+fun porcentajeKM(): Double {
+    var porcentaje: Double
+    var anosFurgo = llegirInt(BLUE + "Introduce los años de tu furgoneta:" + RESET, 0, 100)
+
+    // Muestra un mensaje indicando los años matriculados de la furgoneta.
+    println(GREEN + "Tu furgoneta lleva matriculada $anosFurgo años" + RESET)
+
+    if (anosFurgo < 6) porcentaje = 0.00001
+    else if (anosFurgo >= 6 && anosFurgo < 10) porcentaje = 0.00002
+    else porcentaje = 0.00004
     return porcentaje
 }
--- src/main/kotlin/funcionsVW.kt
+++ src/main/kotlin/funcionsVW.kt
@@ -1,18 +1,39 @@
 import org.example.BLUE
+import org.example.GREEN
 import org.example.PURPLE
 import org.example.RESET
 
-fun calcularPreuBase(normal:Boolean):Double{
-    val preuBase: Double = when(normal){
-        true -> 73490.0
-        false -> 93490.0
+/**
+ * Calcula el precio base de la furgoneta según la versión.
+ *
+ * @param normal Indica si la furgoneta es de versión normal (true) o versión Camper (false).
+ * @return El precio base de la furgoneta.
+ */
+fun calcularPrecioBase(normal: Boolean): Double {
+    return if (normal) {
+        73490.0
+    } else {
+        93490.0
     }
-    return preuBase
 }
 
-fun calcularPreuActual(preuBase:Double):Double{
-    val kmTotales=llegirLong(BLUE+"Introduce los km de tu furgoneta:"+ RESET,0)
-    println(PURPLE+"Tu furgoneta ha recorrido un total de "+kmTotales+"km"+RESET)
-    val preuActual:Double=preuBase-preuBase*porcentajeKM()*kmTotales
-    return  preuActual-depreciacionEstadoNeumaticos()
+/**
+ * Calcula el precio actual de la furgoneta.
+ *
+ * @param precioBase El precio base de la furgoneta.
+ * @return El precio actual de la furgoneta, considerando descuentos y depreciación.
+ */
+fun calcularPrecioActual(precioBase: Double): Double {
+    // Se solicita al usuario ingresar los kilómetros totales recorridos por la furgoneta.
+    val kmTotales = llegirLong(BLUE + "Introduce los km de tu furgoneta:" + RESET, 0)
+    // Se muestra un mensaje indicando los kilómetros recorridos por la furgoneta.
+    println(GREEN + "Tu furgoneta ha recorrido un total de $kmTotales km" + RESET)
+
+    // Se calcula el porcentaje de descuento basado en los kilómetros totales recorridos.
+    val porcentajeDescuento = porcentajeKM()
+    // Se calcula la depreciación de los neumáticos.
+    val depreciacion = depreciacionEstadoNeumaticos()
+
+    // Se devuelve el precio actual restando descuentos y depreciación al precio base.
+    return precioBase - precioBase * porcentajeDescuento * kmTotales - depreciacion
 }
